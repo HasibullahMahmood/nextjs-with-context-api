66392a0651d587f0f2dac26fe89439fd4d66acd9
Hasibullah Mahmood 2 <hasibullahmahmod@gmail.com>
2021-09-30 09:34:42 +0300

    next config file implemented

--- helpers/db-util.js
+++ helpers/db-util.js
@@ -1,9 +1,9 @@
 import { MongoClient } from 'mongodb';
 
 export async function connectDatabase() {
-	const client = await MongoClient.connect(
-		'mongodb+srv://hasibullah:nodecomplete@cluster0.4gnac.mongodb.net/events?retryWrites=true&w=majority'
-	);
+	const url = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_cluster}.4gnac.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
+
+	const client = await MongoClient.connect(url);
 
 	return client;
 }
---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	env: {
+		mongodb_username: 'hasibullah',
+		mongodb_password: 'nodecomplete',
+		mongodb_cluster: 'cluster0',
+		mongodb_database: 'events',
+	},
+};
